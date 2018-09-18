a933be41fb62c87b04d2328c8c1ba419f7570d41
Victor Dibia <chuvidi2003@gmail.com>
2018-09-18 07:21:19 -0400

    add some comments

--- detect_multi_threaded.py
+++ detect_multi_threaded.py
@@ -1,4 +1,4 @@
-from utils import detector_utils as detector_utils 
+from utils import detector_utils as detector_utils
 import cv2
 import tensorflow as tf
 import multiprocessing
@@ -8,12 +8,12 @@ from utils.detector_utils import WebcamVideoStream
 import datetime
 import argparse
 
-
 frame_processed = 0
 score_thresh = 0.2
 
 # Create a worker thread that loads graph and
 # does detection on images in an input queue and puts it on an output queue
+
 
 def worker(input_q, output_q, cap_params, frame_processed):
     print(">> loading frozen model for worker")
@@ -23,12 +23,17 @@ def worker(input_q, output_q, cap_params, frame_processed):
         #print("> ===== in worker loop, frame ", frame_processed)
         frame = input_q.get()
         if (frame is not None):
-            # actual detection
+            # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
+            # while scores contains the confidence for each of these boxes.
+            # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
+
             boxes, scores = detector_utils.detect_objects(
                 frame, detection_graph, sess)
             # draw bounding boxes
             detector_utils.draw_box_on_image(
-                cap_params['num_hands_detect'], cap_params["score_thresh"], scores, boxes, cap_params['im_width'], cap_params['im_height'], frame)
+                cap_params['num_hands_detect'], cap_params["score_thresh"],
+                scores, boxes, cap_params['im_width'], cap_params['im_height'],
+                frame)
             # add frame annotated with bounding box to queue
             output_q.put(frame)
             frame_processed += 1
@@ -40,30 +45,69 @@ def worker(input_q, output_q, cap_params, frame_processed):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-src', '--source', dest='video_source', type=int,
-                        default=0, help='Device index of the camera.')
-    parser.add_argument('-nhands', '--num_hands', dest='num_hands', type=int,
-                        default=2, help='Max number of hands to detect.')
-    parser.add_argument('-fps', '--fps', dest='fps', type=int,
-                        default=1, help='Show FPS on detection/display visualization')
-    parser.add_argument('-wd', '--width', dest='width', type=int,
-                        default=300, help='Width of the frames in the video stream.')
-    parser.add_argument('-ht', '--height', dest='height', type=int,
-                        default=200, help='Height of the frames in the video stream.')
-    parser.add_argument('-ds', '--display', dest='display', type=int,
-                        default=1, help='Display the detected images using OpenCV. This reduces FPS')
-    parser.add_argument('-num-w', '--num-workers', dest='num_workers', type=int,
-                        default=4, help='Number of workers.')
-    parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
-                        default=5, help='Size of the queue.')
+    parser.add_argument(
+        '-src',
+        '--source',
+        dest='video_source',
+        type=int,
+        default=0,
+        help='Device index of the camera.')
+    parser.add_argument(
+        '-nhands',
+        '--num_hands',
+        dest='num_hands',
+        type=int,
+        default=2,
+        help='Max number of hands to detect.')
+    parser.add_argument(
+        '-fps',
+        '--fps',
+        dest='fps',
+        type=int,
+        default=1,
+        help='Show FPS on detection/display visualization')
+    parser.add_argument(
+        '-wd',
+        '--width',
+        dest='width',
+        type=int,
+        default=300,
+        help='Width of the frames in the video stream.')
+    parser.add_argument(
+        '-ht',
+        '--height',
+        dest='height',
+        type=int,
+        default=200,
+        help='Height of the frames in the video stream.')
+    parser.add_argument(
+        '-ds',
+        '--display',
+        dest='display',
+        type=int,
+        default=1,
+        help='Display the detected images using OpenCV. This reduces FPS')
+    parser.add_argument(
+        '-num-w',
+        '--num-workers',
+        dest='num_workers',
+        type=int,
+        default=4,
+        help='Number of workers.')
+    parser.add_argument(
+        '-q-size',
+        '--queue-size',
+        dest='queue_size',
+        type=int,
+        default=5,
+        help='Size of the queue.')
     args = parser.parse_args()
 
     input_q = Queue(maxsize=args.queue_size)
     output_q = Queue(maxsize=args.queue_size)
 
-    video_capture = WebcamVideoStream(src=args.video_source,
-                                      width=args.width,
-                                      height=args.height).start()
+    video_capture = WebcamVideoStream(
+        src=args.video_source, width=args.width, height=args.height).start()
 
     cap_params = {}
     frame_processed = 0
@@ -96,8 +140,7 @@ if __name__ == '__main__':
 
         output_frame = cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR)
 
-        elapsed_time = (datetime.datetime.now() -
-                        start_time).total_seconds()
+        elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
         num_frames += 1
         fps = num_frames / elapsed_time
         # print("frame ",  index, num_frames, elapsed_time, fps)
@@ -105,8 +148,8 @@ if __name__ == '__main__':
         if (output_frame is not None):
             if (args.display > 0):
                 if (args.fps > 0):
-                    detector_utils.draw_fps_on_image(
-                        "FPS : " + str(int(fps)), output_frame)
+                    detector_utils.draw_fps_on_image("FPS : " + str(int(fps)),
+                                                     output_frame)
                 cv2.imshow('Multi-Threaded Detection', output_frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
@@ -115,13 +158,12 @@ if __name__ == '__main__':
                     num_frames = 0
                     start_time = datetime.datetime.now()
                 else:
-                    print("frames processed: ",  index,
-                          "elapsed time: ", elapsed_time, "fps: ", str(int(fps)))
+                    print("frames processed: ", index, "elapsed time: ",
+                          elapsed_time, "fps: ", str(int(fps)))
         else:
             # print("video end")
             break
-    elapsed_time = (datetime.datetime.now() -
-                    start_time).total_seconds()
+    elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
     fps = num_frames / elapsed_time
     print("fps", fps)
     pool.terminate()
--- detect_single_threaded.py
+++ detect_single_threaded.py
@@ -10,22 +10,61 @@ detection_graph, sess = detector_utils.load_inference_graph()
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-sth', '--scorethreshold', dest='score_thresh', type=float,
-                        default=0.2, help='Score threshold for displaying bounding boxes')
-    parser.add_argument('-fps', '--fps', dest='fps', type=int,
-                        default=1, help='Show FPS on detection/display visualization')
-    parser.add_argument('-src', '--source', dest='video_source',
-                        default=0, help='Device index of the camera.')
-    parser.add_argument('-wd', '--width', dest='width', type=int,
-                        default=320, help='Width of the frames in the video stream.')
-    parser.add_argument('-ht', '--height', dest='height', type=int,
-                        default=180, help='Height of the frames in the video stream.')
-    parser.add_argument('-ds', '--display', dest='display', type=int,
-                        default=1, help='Display the detected images using OpenCV. This reduces FPS')
-    parser.add_argument('-num-w', '--num-workers', dest='num_workers', type=int,
-                        default=4, help='Number of workers.')
-    parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
-                        default=5, help='Size of the queue.')
+    parser.add_argument(
+        '-sth',
+        '--scorethreshold',
+        dest='score_thresh',
+        type=float,
+        default=0.2,
+        help='Score threshold for displaying bounding boxes')
+    parser.add_argument(
+        '-fps',
+        '--fps',
+        dest='fps',
+        type=int,
+        default=1,
+        help='Show FPS on detection/display visualization')
+    parser.add_argument(
+        '-src',
+        '--source',
+        dest='video_source',
+        default=0,
+        help='Device index of the camera.')
+    parser.add_argument(
+        '-wd',
+        '--width',
+        dest='width',
+        type=int,
+        default=320,
+        help='Width of the frames in the video stream.')
+    parser.add_argument(
+        '-ht',
+        '--height',
+        dest='height',
+        type=int,
+        default=180,
+        help='Height of the frames in the video stream.')
+    parser.add_argument(
+        '-ds',
+        '--display',
+        dest='display',
+        type=int,
+        default=1,
+        help='Display the detected images using OpenCV. This reduces FPS')
+    parser.add_argument(
+        '-num-w',
+        '--num-workers',
+        dest='num_workers',
+        type=int,
+        default=4,
+        help='Number of workers.')
+    parser.add_argument(
+        '-q-size',
+        '--queue-size',
+        dest='queue_size',
+        type=int,
+        default=5,
+        help='Size of the queue.')
     args = parser.parse_args()
 
     cap = cv2.VideoCapture(args.video_source)
@@ -49,32 +88,35 @@ if __name__ == '__main__':
         except:
             print("Error converting to RGB")
 
-        # actual detection
-        boxes, scores = detector_utils.detect_objects(
-            image_np, detection_graph, sess)
+        # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
+        # while scores contains the confidence for each of these boxes.
+        # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
 
-        # draw bounding boxes
-        detector_utils.draw_box_on_image(
-            num_hands_detect, args.score_thresh, scores, boxes, im_width, im_height, image_np)
+        boxes, scores = detector_utils.detect_objects(image_np,
+                                                      detection_graph, sess)
+
+        # draw bounding boxes on frame
+        detector_utils.draw_box_on_image(num_hands_detect, args.score_thresh,
+                                         scores, boxes, im_width, im_height,
+                                         image_np)
 
         # Calculate Frames per second (FPS)
         num_frames += 1
-        elapsed_time = (datetime.datetime.now() -
-                        start_time).total_seconds()
+        elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
         fps = num_frames / elapsed_time
 
         if (args.display > 0):
             # Display FPS on frame
             if (args.fps > 0):
-                detector_utils.draw_fps_on_image(
-                    "FPS : " + str(int(fps)), image_np)
+                detector_utils.draw_fps_on_image("FPS : " + str(int(fps)),
+                                                 image_np)
 
-            cv2.imshow('Single-Threaded Detection', cv2.cvtColor(
-                image_np, cv2.COLOR_RGB2BGR))
+            cv2.imshow('Single-Threaded Detection',
+                       cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 break
         else:
-            print("frames processed: ",  num_frames,
-                  "elapsed time: ", elapsed_time, "fps: ", str(int(fps)))
+            print("frames processed: ", num_frames, "elapsed time: ",
+                  elapsed_time, "fps: ", str(int(fps)))
